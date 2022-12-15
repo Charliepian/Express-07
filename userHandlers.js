adf09c5c819f1809742b00f97c5bcb64c4831b6d
charliepian <charlie.piancatelli@gmail.com>
2022-12-15 11:59:55 +0100

    Express8

--- userHandlers.js
+++ userHandlers.js
@@ -1,7 +1,9 @@
 const database = require("./database");
 
+// firstname, lastname, email, language, password, city
+
 const getUsers = (req, res) => {
-  const initialSql = "select firstname, lastname, email, language, city from users";
+  const initialSql = "select * from users";
   const where = [];
 
   if (req.query.city != null) {
@@ -113,10 +115,31 @@ const deleteUser = (req, res) => {
     });
 };
 
+const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
+  const { email } = req.body;
+
+  database
+    .query("select * from users where email = ?", [email])
+    .then(([users]) => {
+      if (users[0] != null) {
+        req.user = users[0];
+
+        next();
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
 module.exports = {
   getUsers,
   getUserById,
   postUser,
   updateUser,
   deleteUser,
+  getUserByEmailWithPasswordAndPassToNext,
 };
